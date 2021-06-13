f713fe5fac2cb2dde42a0357401f53aa63b9b5be
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-13 15:05:23 +0100

    passing dynamic content

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
         <h1>Hi, Im a React App</h1>
-        <Person />
+        <Person name="Bex" age="28"/>
+        <Person name="Becky" age="34">My Hobbies: Coding</Person>
+        <Person name="Bexter" age="26"/>
     </div>
   );
 }
