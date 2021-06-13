ef03376c9cc3c41eafd3586718b0d0fb0037af10
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-13 14:56:41 +0100

    added a component to the main App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hi, Im a React App</h1>
-  //   </div>
-  // );
-    return React.createElement('div', {className: 'App'},  React.createElement('h1', null,'Hi, I\'m a React App'));
+  return (
+    <div className="App">
+      <h1>Hi, Im a React App</h1>
+        <Person />
+    </div>
+  );
 }
 
 export default App;
