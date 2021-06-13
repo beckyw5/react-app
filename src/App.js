4041916cbe58671aca12f6efb89f853237dc650e
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-13 15:34:57 +0100

    using the state property to pass information

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component{
+    state = {
+        persons: [
+            { name: 'Bex', age: 28},
+            { name: 'Becky', age: 34},
+            { name: 'Bexter', age: 26}
+        ]
+    }
 
     render() {
         return (
             <div className="App">
                 <h1>Hi, Im a React App</h1>
                 <button>Switch Name</button>
-                <Person name="Bex" age="28"/>
-                <Person name="Becky" age="34">My Hobbies: Coding</Person>
-                <Person name="Bexter" age="26"/>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
             </div>
         );
     }
