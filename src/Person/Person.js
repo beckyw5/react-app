57be84507fba4eb95665a4df6f04c38e9161adb6
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-13 15:21:25 +0100

    Added if children are set, render child element in JSX

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>Hi, My name is {props.name} and I am {props.age} years old.</p>
+    return (
+        <div>
+            <p>Hi, My name is {props.name} and I am {props.age} years old.</p>
+            {props.children && <p>{props.children}</p>}
+        </div>
+    )
 }
 
 export default person;
