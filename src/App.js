5912f21738d87266de8c55d824d8a864dc0b0087
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-13 15:30:23 +0100

    Changed App function to App Class which extends Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hi, Im a React App</h1>
-        <Person name="Bex" age="28"/>
-        <Person name="Becky" age="34">My Hobbies: Coding</Person>
-        <Person name="Bexter" age="26"/>
-    </div>
-  );
+class App extends Component{
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, Im a React App</h1>
+                <button>Switch Name</button>
+                <Person name="Bex" age="28"/>
+                <Person name="Becky" age="34">My Hobbies: Coding</Person>
+                <Person name="Bexter" age="26"/>
+            </div>
+        );
+    }
 }
 
 export default App;
