a360db44e2e5c29340755f3ecd0a8f83d5b08778
Becky Wilson <rebecca.wilson2@sky.uk>
2021-06-14 20:00:46 +0100

    rendering persons by looping through state.persons

--- src/App.js
+++ src/App.js
@@ -53,17 +53,11 @@ class App extends Component {
         if (this.state.showPersons) {
             persons = (
                 <div className="app__person">
-                    <Person
-                        name={this.state.persons[0].name}
-                        age={this.state.persons[0].age} />
-                    <Person
-                        name={this.state.persons[1].name}
-                        age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, "Rebecca")}
-                        changed={this.nameChangedHandler}/>
-                    <Person
-                        name={this.state.persons[2].name}
-                        age={this.state.persons[2].age} />
+                    { this.state.persons.map(person => {
+                        return <Person
+                            name={person.name}
+                            age={person.age} />
+                    }) }
                 </div>
             );
         }
